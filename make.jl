b8cd272a7cdcb206b72a3ee81448f489b0bf699f
Cameron Pfiffer <cpfiffer@gmail.com>
2020-09-27 13:25:10 -0700

    Minor fix to versioning

--- make.jl
+++ make.jl
@@ -33,46 +33,15 @@ in("no-tutorials", ARGS) || copy_tutorial(tutorial_path)
 # set default baseurl for the master branch
 baseurl = "/turing.ml/" * ENV["TURING_VERSION"]
 
+@info "" baseurl
+
 # deploy
 devurl = "dev"
-tags = tag_list(GitRepo(package_directory))
-versions = VersionNumber.(tags)
-
-# Find the most recent tag
-function highest_tags(versions)
-    majors = unique(map(x -> x.major, versions))
-    tags_to_use = []
-
-    for major in majors
-        filtered = filter(x -> x.major == major, versions)
-        minors = unique(map(x -> x.minor, filtered))
-
-        for minor in minors
-            minor_filtered = sort(filter(x -> x.minor == minor, filtered))
-            
-            push!(tags_to_use, minor_filtered[end])
-        end
-    end
-    return sort(tags_to_use)
-end
-
-highest = maximum(highest_tags(versions))
-
-# set baseurl for version tag when current head is tagged
-vtag = "v" * string(highest)
-
-# version_match = match(r"^(v\d+\.\d+\.\d+)$", vtag)
-# if !isnothing(version_match)
-#     baseurl = "/" * version_match[1]
-# end
 
 jekyll_build = joinpath(@__DIR__, "jekyll-build")
 with_baseurl(() -> run(`$jekyll_build`), baseurl)
 
-repo = "github.com:TuringLang/Turing.jl.git"
-
-# Lying to GitHub
-# ENV["GITHUB_REPOSITORY"] = "TuringLang/Turing.jl"
+# repo = "github.com:TuringLang/Turing.jl.git"
 
 deploydocs(
     target = "_site",
