944e2e9e000050422a4095e4aaa8665a5dcc30e2
Cameron Pfiffer <cpfiffer@gmail.com>
2020-10-11 14:04:44 -0700

    Handle versioning better

--- make.jl
+++ make.jl
@@ -7,6 +7,17 @@ using LibGit2: clone, tag_list, GitRepo
 # Include the utility functions.
 include("make-utils.jl")
 
+# Get the version number
+version = if haskey(ENV, "TURING_VERSION")
+    ENV["TURING_VERSION"]
+else
+    if length(ARGS) > 0
+        ARGS[1]
+    else
+        error("Version unknown")
+    end
+end
+
 # Find the package directory
 package_directory = dirname(dirname(pathof(Turing)))
 source_path = joinpath(package_directory, "docs", "src")
