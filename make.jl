685cec094f06e0e6bd705572341694763fa7c946
Cameron Pfiffer <cpfiffer@gmail.com>
2020-10-27 18:40:47 -0700

    Add deploy config

--- make.jl
+++ make.jl
@@ -3,17 +3,19 @@ Pkg.activate(".")
 
 using Documenter, DocumenterMarkdown, Turing, AdvancedHMC, Bijectors, AdvancedMH
 using LibGit2: clone, tag_list, GitRepo
+using Documenter: GithubActions
 
 # Include the utility functions.
 include("make-utils.jl")
 
 # Get the version number
 trim_version(x) = x[1:findlast('.', x) - 1]
-version = if haskey(ENV, "TURING_VERSION")
-    ENV["TURING_VERSION"]
+
+version, is_dev = if haskey(ENV, "TURING_VERSION")
+    ENV["TURING_VERSION"], true
 else
     if length(ARGS) > 0
-        trim_version(ARGS[1])
+        trim_version(ARGS[1]), false
     else
         error("Version unknown")
     end
@@ -50,12 +52,20 @@ baseurl = "/turing.ml/" * version
 # deploy
 jekyll_build = joinpath(@__DIR__, "jekyll-build")
 with_baseurl(() -> run(`$jekyll_build`), baseurl)
+repo = "github.com:cpfiffer/turing.ml.git"
+
+deploy_config = GithubActions(
+    "cpfiffer/turing.ml", #github_repository::String
+    "push", #github_event_name::String
+    is_dev ? "refs/tags/master" : "refs/tags/$(ARGS[1])" #github_ref::String
+)
 
 deploydocs(
     target = "_site",
-    repo = "github.com:cpfiffer/turing.ml.git",
+    repo = repo,
     branch = "gh-pages",
     devbranch = "master",
     devurl = "dev",
-    versions = ["stable" => "v^", "v#.#", "dev" => "dev"]
+    versions = ["stable" => "v^", "v#.#", "dev" => "dev"],
+    deploy_config = deploy_config
 )
