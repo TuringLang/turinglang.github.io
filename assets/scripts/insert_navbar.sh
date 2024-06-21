a7c1f472a250279ff533a7d9a46e8c25dd0c7b09
Shravan Goswami <123811742+shravanngoswamii@users.noreply.github.com>
2024-06-21 19:55:20 +0530

    added script and html for Documenter Navigation (#77)
    * added script and html for Documenter Navigation
    
    * fixed NAVBAR URL
    
    * Update assets/scripts/insert_navbar.sh
    
    * Create README.md
    
    * removed extra line
    
    * Update assets/scripts/navbar.html
    
    Co-authored-by: Hong Ge <3279477+yebai@users.noreply.github.com>
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Hong Ge <3279477+yebai@users.noreply.github.com>

---
+++ assets/scripts/insert_navbar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script inserts a top navigation bar (e.g., `navbar.html`) into Documenter.jl generated sites. 
+# The resulting output is similar to MultiDocumenter's navigation menu. The navigation menu is 
+# hard-coded at the moment, which could be improved in the future. 
+
+# URL of the navigation bar HTML file
+NAVBAR_URL="https://raw.githubusercontent.com/TuringLang/turinglang.github.io/main/assets/scripts/navbar.html"
+
+# Directory containing HTML files (passed as the first argument to the script)
+HTML_DIR=$1
+
+# Download the navigation bar HTML content
+NAVBAR_HTML=$(curl -s $NAVBAR_URL)
+
+# Check if the download was successful
+if [ -z "$NAVBAR_HTML" ]; then
+    echo "Failed to download navbar HTML"
+    exit 1
+fi
+
+# Process each HTML file in the directory
+for file in $(find $HTML_DIR -name "*.html"); do
+    # Read the contents of the HTML file
+    file_contents=$(cat "$file")
+
+    # Insert the navbar HTML after the <body> tag
+    updated_contents="${file_contents/$'<body>'/$'<body>\n'$NAVBAR_HTML}"
+
+    # Write the updated contents back to the file
+    echo "$updated_contents" > "$file"
+    echo "Updated $file"
+done
